cd7fdd080f6711b45286a1384fe078db05d72ffd
Jesús R. Loza <jesusr.loza@gmail.com>
2025-11-06 15:01:41 -0600

    Add server and preview configuration to Vite for local development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server is used if you ever run vite dev locally
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+    // Optional, only needed if you run `vite dev` through Cloudflare
+    allowedHosts: ['qr.dgtipeautomations.work'],
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['qr.dgtipeautomations.work'],
+  },
 })
